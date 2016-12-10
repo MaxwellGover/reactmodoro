f55131688c70ffb4802f2e8900a8ee4cacf7d209
Maxwell Gover <maxwellrgover@gmail.com>
2016-12-09 20:22:33 -0500

    Initial commit

---
+++ app/containers/App/AppContainer.js
@@ -0,0 +1,19 @@
+import React, { PropTypes, Component } from 'react'
+import { View } from 'react-native'
+import { ReactModoroNavigator } from '~/containers'
+import { PreSplash } from '~/components'
+
+export default class AppContainer extends Component {
+  static propTypes = {
+   // isAuthenticating: PropTypes.bool.isRequired
+  }
+  render () {
+    return (
+      <View style={{flex: 1}}>
+        {this.props.isAuthenticating === true 
+        	? <PreSplash/> 
+        	: <ReactModoroNavigator /> }
+      </View>
+    )
+  }
+}
